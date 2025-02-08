64beaf48658070f35367afca0134edbd83a10130
Amir3zz <hunicastt@gmail.com>
2025-02-08 22:07:10 +0700

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BankAccount account = new BankAccount("4937503927", "Camila", 2500000);
+        account.getAccountInfo();
+        account.checkBalance();
+        account.deposit(54839);
+        account.withdraw(92045);
+        account.checkBalance();
+
+        SavingsAccount savingsAccount = new SavingsAccount("4937503927", "Camila", 19475000, 0.02);
+        savingsAccount.checkBalance();
+        savingsAccount.applyInterest();
+        savingsAccount.checkBalance();
     }
 }
