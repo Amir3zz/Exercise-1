3dea5e717af4569ab8609adb670207e290fc0697
Amir3zz <hunicastt@gmail.com>
2025-02-08 22:07:34 +0700

    BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+    String accountNumber;
+    String accountHolderName;
+    int balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, int initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: Rp " + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: Rp " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("Account Balance: Rp " + balance);
+    }
+
+    public void getAccountInfo(){
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder Name: " + accountHolderName);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
